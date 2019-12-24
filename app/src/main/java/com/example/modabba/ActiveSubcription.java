00f514f6d5c0346baa6d6fab161980da8ab6a4b4
doyel <doyelsaha99@gmail.com>
2019-12-24 22:19:32 +0530

    changes

---
+++ app/src/main/java/com/example/modabba/ActiveSubcription.java
@@ -0,0 +1,52 @@
+package com.example.modabba;
+
+public class ActiveSubcription {
+    private String subcriptionid;
+    private String plan_name;
+    private String start_date;
+    private String end_date;
+
+    public  ActiveSubcription()
+    {
+
+    }
+
+    public ActiveSubcription(String subcriptionid, String plan_name, String start_date, String end_date) {
+        this.subcriptionid = subcriptionid;
+        this.plan_name = plan_name;
+        this.start_date = start_date;
+        this.end_date = end_date;
+    }
+
+    public String getSubcriptionid() {
+        return subcriptionid;
+    }
+
+    public void setSubcriptionid(String subcriptionid) {
+        this.subcriptionid = subcriptionid;
+    }
+
+    public String getPlan_name() {
+        return plan_name;
+    }
+
+    public void setPlan_name(String plan_name) {
+        this.plan_name = plan_name;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+}
---
+++ app/src/main/java/com/example/modabba/ActiveSubcriptionAdapter.java
@@ -0,0 +1,66 @@
+package com.example.modabba;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ActiveSubcriptionAdapter extends RecyclerView.Adapter<ActiveSubcriptionAdapter.RecycleViewHolder>{
+
+    Context context;
+    List<ActiveSubcription> subcriptionList;
+
+    public ActiveSubcriptionAdapter(Context context, List<ActiveSubcription> subcriptionList) {
+        this.context = context;
+        this.subcriptionList = subcriptionList;
+    }
+
+    @NonNull
+    @Override
+    public RecycleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        view= LayoutInflater.from(context).inflate(R.layout.subscription_single_item,parent,false);
+        RecycleViewHolder recycleViewHolder=new RecycleViewHolder(view);
+        return recycleViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecycleViewHolder holder, int position) {
+        ActiveSubcription activeSubcription=subcriptionList.get(position);
+        holder.subid.setText(activeSubcription.getSubcriptionid());
+        holder.plan_name.setText(activeSubcription.getPlan_name());
+        holder.start_date.setText(activeSubcription.getStart_date());
+        holder.end_date.setText(activeSubcription.getEnd_date());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return subcriptionList.size();
+    }
+
+    public static class RecycleViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView subid,plan_name,start_date,end_date;
+        Button unsubcribe_btn,modify_btn,skip_btn;
+
+        public RecycleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            subid=itemView.findViewById(R.id.subid);
+            plan_name=itemView.findViewById(R.id.plan_name);
+            start_date=itemView.findViewById(R.id.start);
+            end_date=itemView.findViewById(R.id.end);
+            unsubcribe_btn=itemView.findViewById(R.id.more_btn);
+            modify_btn=itemView.findViewById(R.id.modify);
+            skip_btn=itemView.findViewById(R.id.skip);
+        }
+    }
+}
