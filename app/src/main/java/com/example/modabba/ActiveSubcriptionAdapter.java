888d51d11e58a15dce44ef77b109c89db7434c11
doyel <doyelsaha99@gmail.com>
2020-01-03 12:29:49 +0530

    skip and pause

--- app/src/main/java/com/example/modabba/ActiveSubcriptionAdapter.java
+++ app/src/main/java/com/example/modabba/ActiveSubcriptionAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+
 import java.util.List;
 
 public class ActiveSubcriptionAdapter extends RecyclerView.Adapter<ActiveSubcriptionAdapter.RecycleViewHolder>{
@@ -32,12 +34,40 @@ public class ActiveSubcriptionAdapter extends RecyclerView.Adapter<ActiveSubcrip
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecycleViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final RecycleViewHolder holder, int position) {
         ActiveSubcription activeSubcription=subcriptionList.get(position);
         holder.subid.setText(activeSubcription.getSubcriptionid());
         holder.plan_name.setText(activeSubcription.getPlan_name());
         holder.start_date.setText(activeSubcription.getStart_date());
         holder.end_date.setText(activeSubcription.getEnd_date());
+        //counter for skip meal
+        holder.skip_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.btn1.setVisibility(View.VISIBLE);
+                holder.skip_btn.setVisibility(View.GONE);
+                holder.btn1.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String skipnum = holder.btn1.getNumber();
+                    }
+                });
+            }
+        });
+        //counter for pause meal
+        holder.pause_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.btn2.setVisibility(View.VISIBLE);
+                holder.pause_btn.setVisibility(View.GONE);
+                holder.btn2.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String pausenum = holder.btn2.getNumber();
+                    }
+                });
+            }
+        });
 
 
 
@@ -53,6 +83,7 @@ public class ActiveSubcriptionAdapter extends RecyclerView.Adapter<ActiveSubcrip
     {
         TextView subid,plan_name,start_date,end_date;
         Button pause_btn,skip_btn;
+        ElegantNumberButton btn1,btn2;
 
         public RecycleViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -62,6 +93,8 @@ public class ActiveSubcriptionAdapter extends RecyclerView.Adapter<ActiveSubcrip
             end_date=itemView.findViewById(R.id.end);
             pause_btn=itemView.findViewById(R.id.pause);
             skip_btn=itemView.findViewById(R.id.skip);
+            btn1=itemView.findViewById(R.id.number_btn1);
+            btn2=itemView.findViewById(R.id.number_btn2);
         }
     }
 }
